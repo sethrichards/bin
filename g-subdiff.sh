5a85b92c6612d44c8bc8b391b7054dfba61be171
sethrichards <seth@sethrichards.com>
2017-08-23 11:06:14 -0400

    Adding comments to g-subdiff.sh

--- g-subdiff.sh
+++ g-subdiff.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
+# Generate .diff files for current Git repo and all submodules.
 # From Stack Overflow, https://stackoverflow.com/questions/36646323/generate-patch-of-uncommitted-changes-for-git-repo-and-submodules
 #
 #git submodule --quiet foreach --recursive 'export NAME="${PWD##*/}"; git --no-pager diff --src-prefix="a/${NAME}/" --dst-prefix="b/${NAME}/"'
 #
 
 export STARTDIR=${PWD}
-echo ${STARTDIR}
+echo Generating .diffs for all submodules in ${STARTDIR}
 git submodule --quiet foreach --recursive 'export NAME="${PWD##*/}"; \
                                            git --no-pager diff > ${STARTDIR}/${NAME}.diff'
