4226e3c213da3ba1b7c6e40f3ae66ae20d91f83a
Seth Richards <5908913+sethrichards@users.noreply.github.com>
2018-05-25 10:19:51 -0400

    GCC colorization, and add a nice \[\e[32;49m\]\u\[\e[m\]\[\e[32;49m\]@\[\e[m\]\[\e[32;49m\]\h\[\e[m\]\[\e[32;49m\]:\[\e[m\]\[\e[34;49m\]\w\[\e[m\]\[\e[49m\] \[\e[m\]\[\e[36;49m\]`parse_git_branch`\[\e[m\]\n->

--- bashrc-include.sh
+++ bashrc-include.sh
@@ -64,3 +64,63 @@ alias g-substatus="git status -s; git submodule foreach 'git status -s'"
 alias g-substatus-u="git status -s -uno; git submodule foreach 'git status -s -uno'"
 alias g-substatus-i="git status -s --ignored; git submodule foreach 'git status -s --ignored'"
 alias g-subclean="git clean -fdx; git submodule foreach 'git clean -fdx'"
+
+# GCC colorization
+# Add -fdiagnostics-color=[auto|never|always] to GCC_FLAGS.
+# See here: https://xaizek.github.io/2015-10-25/enable-colorization-of-gcc-output/
+export GCC_COLORS='error=01;31:warning=01;35:note=01;36:caret=01;32:locus=01:quote=01'
+
+# Add a nice prompt with Git status
+# get current branch in git repo
+function parse_git_branch() {
+    BRANCH=`git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/'`
+    if [ ! "${BRANCH}" == "" ]
+    then
+        STAT=`parse_git_dirty`
+        echo "[${BRANCH}${STAT}]"
+    else
+        echo ""
+    fi
+}
+
+# get current status of git repo
+function parse_git_dirty {
+    status=`git status 2>&1 | tee`
+    dirty=`echo -n "${status}" 2> /dev/null | grep "modified:" &> /dev/null; echo "$?"`
+    untracked=`echo -n "${status}" 2> /dev/null | grep "Untracked files" &> /dev/null; echo "$?"`
+    ahead=`echo -n "${status}" 2> /dev/null | grep "Your branch is ahead of" &> /dev/null; echo "$?"`
+    newfile=`echo -n "${status}" 2> /dev/null | grep "new file:" &> /dev/null; echo "$?"`
+    renamed=`echo -n "${status}" 2> /dev/null | grep "renamed:" &> /dev/null; echo "$?"`
+    deleted=`echo -n "${status}" 2> /dev/null | grep "deleted:" &> /dev/null; echo "$?"`
+    bits=''
+    if [ "${renamed}" == "0" ]; then
+        bits=">${bits}"
+    fi
+    if [ "${ahead}" == "0" ]; then
+        bits="*${bits}"
+    fi
+    if [ "${newfile}" == "0" ]; then
+        bits="+${bits}"
+    fi
+    if [ "${untracked}" == "0" ]; then
+        bits="?${bits}"
+    fi
+    if [ "${deleted}" == "0" ]; then
+        bits="x${bits}"
+    fi
+    if [ "${dirty}" == "0" ]; then
+        bits="!${bits}"
+    fi
+    if [ ! "${bits}" == "" ]; then
+        echo " ${bits}"
+    else
+        echo ""
+    fi
+}
+
+#export PS1="\u@\h:\w\ \`parse_git_branch\`-> "
+#export PS1="\[\e[30;42m\]\u\[\e[m\]\[\e[30;42m\]@\[\e[m\]\[\e[30;42m\]\h\[\e[m\]\[\e[30;42m\]:\[\e[m\]\[\e[30;44m\]\w\[\e[m\]\[\e[44m\] \[\e[m\]\[\e[30;46m\]\`parse_git_branch\`\[\e[m\]\n-> "
+
+#export PS1="\[\e[32;40m\]\u\[\e[m\]\[\e[32;40m\]@\[\e[m\]\[\e[32;40m\]\h\[\e[m\]\[\e[32;40m\]:\[\e[m\]\[\e[34;40m\]\w\[\e[m\]\[\e[40m\] \[\e[m\]\[\e[36;40m\]\`parse_git_branch\`\[\e[m\]\n-> "
+
+export PS1="\[\e[32;49m\]\u\[\e[m\]\[\e[32;49m\]@\[\e[m\]\[\e[32;49m\]\h\[\e[m\]\[\e[32;49m\]:\[\e[m\]\[\e[34;49m\]\w\[\e[m\]\[\e[49m\] \[\e[m\]\[\e[36;49m\]\`parse_git_branch\`\[\e[m\]\n-> "
